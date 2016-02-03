ec23e76ac0da11721a415c67d4741864e5fae6c9
David G Chung <davidc4747@yahoo.com>
2016-02-03 12:00:11 -0500

    Created Functionality and Unit tests for the SignIn Module

---
+++ src/views/SignIn/signin.ctrl.js
@@ -0,0 +1,28 @@
+/*
+* @Author: David
+* @Date:   2016-01-29 13:04:37
+* @Last Modified by:   David
+* @Last Modified time: 2016-02-03 10:38:01
+*/
+
+angular.module('jobTrack.SignIn', ['Repository'])
+    .controller('SignInCtrl', ['$scope', '$location', 'Repository', function($scope, $location, Repository)
+    {
+        $scope.user = { name: '', password: '', remember: false };
+        $scope.errors = [];
+
+        $scope.signIn = function()
+        {
+            //Call Repo.signIn
+            Repository.signIn($scope.user, function (errors)
+            {
+                //Display errors to user
+                $scope.errors = errors;
+
+                //If no errors, Go to CareerClue.MultiJob
+                if(errors.length == 0)
+                    $location.path('/MultiJob');
+            });
+        };
+
+    }]);
---
+++ src/views/SignIn/signin.spec.js
@@ -0,0 +1,70 @@
+/*
+* @Author: David
+* @Date:   2016-02-02 11:18:11
+* @Last Modified by:   David
+* @Last Modified time: 2016-02-03 10:58:55
+*/
+
+describe('SignIn Module', function()
+{
+    // angular vars
+    var $rootScope,
+        $scope,
+        $location,
+        $controller;
+
+    // mock vars
+    var mockUser = { name: 'bob', password: '1234', remember: false };
+    var mockRepo = {
+        signIn: function(user, callback)
+        {
+            var errors = [];
+            if(user.name != mockUser.name && user.password != mockUser.password)
+                errors.push('Invalid user');
+            callback(errors);
+        }
+    }
+
+
+
+    beforeEach(function()
+    {
+        module('jobTrack.SignIn');
+
+        inject(function(_$rootScope_, _$controller_, _$location_)
+        {
+            $rootScope = _$rootScope_;
+            $controller = _$controller_;
+            $location = _$location_;
+        });
+
+        $scope = $rootScope.$new();
+        $controller('SignInCtrl', { '$scope': $scope, 'Repository': mockRepo, '$location': $location });
+    });
+
+
+
+
+
+    it('should should switch screens', function()
+    {
+        spyOn($location, 'path').and.callFake(function(){});
+
+        //Call $scope.signIn with valid user
+        $scope.user = mockUser;
+        $scope.signIn();
+
+        expect($scope.errors.length).toBe(0);
+        expect($location.path).toHaveBeenCalled();
+    });
+
+    it('should should display errors from DB', function()
+    {
+        //Call $scope.signin with invalid user
+        $scope.user = { name: 'qwdqfgqw', password: '12qwfrdfqf34', remember: false };
+        $scope.signIn();
+
+        expect($scope.errors.length).toBeGreaterThan(0);
+    });
+
+});
