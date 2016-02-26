e531042c638da1feacb3f2adee1b05744ed51587
David G Chung <davidc4747@yahoo.com>
2016-02-26 13:34:59 -0500

    Added Sytling that allows MultiJob to reveal .job__notes when a .job is clicked, Also added a search bar to the top of MultiJob

--- src/views/MultiJob/multijob.ctrl.js
+++ src/views/MultiJob/multijob.ctrl.js
@@ -2,12 +2,19 @@
 * @Author: David
 * @Date:   2016-02-04 09:03:26
 * @Last Modified by:   David
-* @Last Modified time: 2016-02-22 14:28:52
+* @Last Modified time: 2016-02-26 12:51:08
 */
 
 angular.module('CareerClue.MultiJob', ['Repository'])
     .controller('MultiJobCtrl', ['$scope', 'Repository', '$routeParams', 'Session' , function($scope, Repository, $routeParams, Session)
     {
         var statusType = $routeParams.statusType;
+
+        $scope.isExpanded = false;
+
+        $scope.toggleExpand = function()
+        {
+            $scope.isExpanded = !$scope.isExpanded;
+        };
 
     }]);
