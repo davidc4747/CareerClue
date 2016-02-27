784c43d83f8e0bfb8d6da38db8fd6afad591e18d
David G Chung <davidc4747@yahoo.com>
2016-02-27 09:19:20 -0500

    Created ground work for MultiJob tests, change the ng-repeat to work based off job objects instead of hard coded html

--- src/views/MultiJob/multijob.ctrl.js
+++ src/views/MultiJob/multijob.ctrl.js
@@ -2,13 +2,24 @@
 * @Author: David
 * @Date:   2016-02-04 09:03:26
 * @Last Modified by:   David
-* @Last Modified time: 2016-02-26 13:49:22
+* @Last Modified time: 2016-02-27 09:04:38
 */
 
 angular.module('CareerClue.MultiJob', ['Repository'])
     .controller('MultiJobCtrl', ['$scope', 'Repository', '$routeParams', 'Session' , function($scope, Repository, $routeParams, Session)
     {
+        // Ask DB for jobs based on status
         var statusType = $routeParams.statusType;
+        $scope.jobs = [];
+
+        // Repo.getJobs('status', callback);
+
+
+
+        $scope.createJob = function()
+        {
+            $scope.jobs.unshift({id: -1});
+        };
 
 
     }]);
---
+++ src/views/MultiJob/multijob.spec.js
@@ -0,0 +1,67 @@
+/*
+* @Author: David
+* @Date:   2016-02-27 08:54:42
+* @Last Modified by:   David
+* @Last Modified time: 2016-02-27 09:17:39
+*/
+
+describe('MultiJob Module', function()
+{
+    // angular vars
+    var $rootScope,
+        $scope,
+        $controller;
+
+    var mockJob = {},
+        mockRepo = {};
+
+
+    beforeEach(function()
+    {
+        module('CareerClue.MultiJob');
+
+        inject(function(_$rootScope_, _$controller_)
+        {
+            // init var
+            $rootScope = _$rootScope_;
+            $controller = _$controller_;
+        });
+
+        // setup controler
+        $scope = $rootScope.$new();
+        $controller('SignInCtrl', { '$scope': $scope, 'Repository': mockRepo});
+    });
+
+
+    describe('OnLoad', function()
+    {
+        xit('should get JobData from DB', function()
+        {
+
+        });
+
+        xit('should add a second search bar to bottom if more than X jobs are in the list', function()
+        {
+
+        });
+    });
+
+
+
+    xit('should create a new job directive when add button is clicked', function()
+    {
+
+    });
+
+    xit('should filter jobs by Company name', function()
+    {
+
+    });
+
+    xit('should pass JobData to job directive', function()
+    {
+
+    });
+
+
+});
