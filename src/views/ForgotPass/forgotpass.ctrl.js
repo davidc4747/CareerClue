b2c83decbb1791865df036054028c87d5bacbd19
David G Chung <davidc4747@yahoo.com>
2016-04-08 08:44:13 -0400

    Combines ForogtPass and ForgotUsername into one Forgot Modules

--- src/views/ForgotPass/forgotpass.ctrl.js
+++
@@ -1,8 +0,0 @@
-/*
-* @Author: David
-* @Date:   2016-04-07 08:40:56
-* @Last Modified by:   David
-* @Last Modified time: 2016-04-07 08:41:14
-*/
-
-'use strict';
--- src/views/ForgotUsername/forgotusername.ctrl.js
+++
@@ -1,8 +0,0 @@
-/*
-* @Author: David
-* @Date:   2016-04-07 08:41:43
-* @Last Modified by:   David
-* @Last Modified time: 2016-04-07 08:41:46
-*/
-
-'use strict';
