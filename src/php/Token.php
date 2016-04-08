990fa80358eec743a5955c7a061aa743d492b90c
David G Chung <davidc4747@yahoo.com>
2016-04-08 10:01:39 -0400

    Create a class that will create and validate tokens

--- src/php/Token.php
+++ src/php/Token.php
@@ -3,25 +3,49 @@
  * @Author: David
  * @Date:   2016-04-07 14:08:48
  * @Last Modified by:   David
- * @Last Modified time: 2016-04-08 08:37:17
+ * @Last Modified time: 2016-04-08 09:37:40
  */
 
-class Token
+// hash_equals replacement for older versions of php
+if(!function_exists('hash_equals'))
 {
-    function generate_token()
+    function hash_equals($str1, $str2)
     {
-        $token = hash('sha256', openssl_random_pseudo_bytes(128), false);
-        // DB->save token
-        return $token;
-    }
-
-    function validate_token($token)
-    {
-        // DB->get user with token
-            // if not exits, return false
-
-        return !empty($result[0]['Token']) && hash_equals($result[0]['Token'], hash('sha256', $token));
+        if(strlen($str1) != strlen($str2))
+        {
+            return false;
+        }
+        else
+        {
+            $res = $str1 ^ $str2;
+            $ret = 0;
+            for($i = strlen($res) - 1; $i >= 0; $i--)
+            {
+                $ret |= ord($res[$i]);
+            }
+            return !$ret;
+        }
     }
 }
 
-$token = new Token();
+// Token Class
+class Token
+{
+    function create($bytes)
+    {
+        $token = hash('sha256', openssl_random_pseudo_bytes($bytes), false);
+        return $token;
+    }
+
+    function validate($raw_token, $valid_token)
+    {
+        // hash raw_token
+        $token = hash('sha256', $raw_token);
+
+        // compare tokens
+        return !empty($valid_token) && hash_equals($valid_token, $token);
+    }
+}
+
+// Initialize the token class
+$cls_token = new Token();
